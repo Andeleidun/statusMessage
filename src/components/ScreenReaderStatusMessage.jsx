f08e5dab907ae3c6420b4ff6de015f721953dd79
Adair Daniels <adairdaniels@gmail.com>
2025-05-15 19:27:23 -0700

    update with requestAnimationFrame, documentation

--- src/components/ScreenReaderStatusMessage.jsx
+++ src/components/ScreenReaderStatusMessage.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 
+// Utility to ensure applications can meet WCAG 2.1 AA SC 4.1.3 Status Messages
+// Which can be found at: https://www.w3.org/WAI/WCAG21/Understanding/status-messages
 export const ScreenReaderStatusMessage = ({ message }) => {
   const [showMessage, setShowMessage] = useState(false);
 
   useEffect(() => {
     if (!showMessage && message) {
-      setShowMessage(true);
+      requestAnimationFrame(() => setShowMessage(true));
     }
   }, [message, showMessage]);
   return (
--- src/components/ScreenReaderStatusMessage.test.jsx
+++ src/components/ScreenReaderStatusMessage.test.jsx
@@ -2,6 +2,8 @@ import { ScreenReaderStatusMessage } from './ScreenReaderStatusMessage';
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 
+// These tests are designed to test against the three tests of
+// WCAG Technique ARIA22, found here: https://www.w3.org/WAI/WCAG21/Techniques/aria/ARIA22.html
 describe('ScreenReaderStatusMessage', () => {
   let message;
 
@@ -9,12 +11,15 @@ describe('ScreenReaderStatusMessage', () => {
     return render(<ScreenReaderStatusMessage message={message} />);
   };
 
+  // 1. Check that the container destined to hold the status message has
+  // a role attribute with a value of status before the status message occurs.
   it('should render empty with undefined message', () => {
     testRender({ message });
     const status = screen.queryByRole('status');
     expect(status).toHaveTextContent('');
   });
 
+  // 2. Check that when the status message is triggered, it is inside the container.
   it('should render passed string status message', () => {
     message = 'status message';
     testRender({ message });
@@ -22,6 +27,9 @@ describe('ScreenReaderStatusMessage', () => {
     expect(status).toHaveTextContent('status message');
   });
 
+  // Check that elements or attributes that provide information equivalent to
+  // the visual experience for the status message (such as a shopping cart image
+  // with proper alt text) also reside in the container.
   it('should render passed JSX status message', () => {
     const number = 7;
     const labelCart = 'items in shopping';
