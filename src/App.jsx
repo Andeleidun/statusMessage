87ffa4c3f8cf6bac650d98b88480cd84628ce46c
Adair Daniels <andeleidun@gmail.com>
2026-08-25 03:05:14 -0700

    refactor: consume the public package entry in demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { ScreenReaderStatusMessage } from './components/ScreenReaderStatusMessage';
+import { ScreenReaderStatusMessage } from './index';
 import './App.css';
 
 function App() {
